8a808847f54519a147b0298e3c6aa1f90a81b8c7
Fabian Bernal <fbernals@aol.com>
2023-09-12 22:03:05 -0700

    Solved prep2.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Enter percentage grade: ");
+        int grade = int.Parse(Console.ReadLine());
+        string letterGrade = "F";
+        string plusMinus = "+";
+
+        if (grade >= 90)
+        {
+           letterGrade = "A"; 
+        }
+        else if (grade >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (grade >= 70)
+        {
+            letterGrade = "c";
+        }
+        else if (grade >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            Console.WriteLine("You failed the course.");
+        }
+
+        if (grade >= 60 && grade <= 93)
+        {
+            if (grade % 10 <= 3)
+            {
+                plusMinus = "-";
+            }
+            else
+            {
+                plusMinus = "+";
+            }
+            
+            
+        }
+        Console.WriteLine($"Your letter grade is {letterGrade}{plusMinus}.");
     }
 }
