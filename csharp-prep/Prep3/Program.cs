a6415b9611acd772de2fe4947a5495cbf4f901c3
Fabian Bernal <fbernals@aol.com>
2023-09-20 16:49:21 -0700

    Solved prep3.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int number;
+        int guessedNumber = 0;
+        string playAgain = null;
+
+        do{
+            Random randomNumber = new Random();
+            number = randomNumber.Next(1,11);
+            while (guessedNumber != number){
+                Console.WriteLine("Guess the number: ");
+                guessedNumber = int.Parse(Console.ReadLine());
+                        
+                if (guessedNumber < number){
+                    Console.WriteLine("Higher.");
+                }
+                else if (guessedNumber > number){
+                    Console.WriteLine("Lower");
+                }
+                else if (guessedNumber == number){
+                    Console.WriteLine("You guessed it!");
+                }
+                
+            }
+            Console.WriteLine("Do you want to play again? (y/n)");
+            playAgain = (Console.ReadLine());
+           
+        } while (playAgain != "n");
+        Console.Write("Good bye!");
+
     }
 }
