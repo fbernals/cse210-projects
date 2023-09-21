ca65f7fa5c7e59827ac1db28fdc2e9432f8429b1
Fabian Bernal <fbernals@aol.com>
2023-09-20 21:30:56 -0700

    Solved prep5 functions.

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        Console.WriteLine("Functions sampler program.");
+        DisplayWelcome();
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+        int squared = SquaredNumber(number); 
+        DisplayResult(name, squared);       
+
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome message.");
+        }
+
+        static string PromptUserName()
+        {
+            Console.WriteLine("What is your name? ");
+            string name = Console.ReadLine();
+            return name;
+        }
+        static int PromptUserNumber()
+        {
+            Console.WriteLine("What is you favorite number? ");
+            int number = int.Parse(Console.ReadLine());
+            return number;
+        }
+        static int SquaredNumber(int number)
+        {
+            return number*number;
+        }
+        static void DisplayResult(string name, int number)
+        {
+            Console.WriteLine($"Hello, {name}. The square of your number is {number}.");
+        }
+
     }
 }
